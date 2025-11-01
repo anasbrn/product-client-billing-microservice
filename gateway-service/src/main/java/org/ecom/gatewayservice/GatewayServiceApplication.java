ca1d489ebde56d6df403acc2314f76eb94a82a5c
BARNOCH Anas <anasbarnoch@gmail.com>
2025-11-01 17:49:15 +0100

    use dynamic routes

--- gateway-service/src/main/java/org/ecom/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/ecom/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,22 @@ package org.ecom.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r1 -> r1.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r2 -> r2.path("/products/**").uri("lb://PRODCUT-SERVICE"))
+                .build();
+    }
 }
