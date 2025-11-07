66bcd78bf60be2445f8188476aa3bb2b0b08eff2
BARNOCH Anas <anasbarnoch@gmail.com>
2025-11-07 22:06:48 +0100

    Add resillience4j for customers service

--- billing-service/src/main/java/org/ecom/billingservice/feign/CustomerServiceRestClient.java
+++ billing-service/src/main/java/org/ecom/billingservice/feign/CustomerServiceRestClient.java
@@ -1,12 +1,18 @@
 package org.ecom.billingservice.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.ecom.billingservice.models.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("customer-service")
+@FeignClient(value = "customer-servicer")
 public interface CustomerServiceRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customer-service", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        return new Customer();
+    }
 }
