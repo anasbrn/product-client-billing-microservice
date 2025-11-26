9a44bf1dbd41c601168fa0a4a7d071b408ff74ea
BARNOCH Anas <anasbarnoch@gmail.com>
2025-11-26 18:04:39 +0100

    fix customer service openFeign bug

--- billing-service/src/main/java/org/ecom/billingservice/feign/CustomerServiceRestClient.java
+++ billing-service/src/main/java/org/ecom/billingservice/feign/CustomerServiceRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "customer-servicer")
+@FeignClient(value = "customer-service")
 public interface CustomerServiceRestClient {
     @GetMapping("/customers/{id}")
     @CircuitBreaker(name = "customer-service", fallbackMethod = "getDefaultCustomer")
